7ad42ecf531cbc23e2cce5982ad87d36baf47ca1
sanmao@123.com <sanmao@123.com>
2022-03-28 19:06:52 -0700

    creation of my desktop project

---
+++ filesUI.py
@@ -0,0 +1,126 @@
+from tkinter import *
+import json
+from tkinter import messagebox
+from utility import find_files
+from datetime import datetime
+import os
+import time
+
+# ---------------------------- function field ------------------------------- #
+
+# ---------------------------- search File Function ------------------------------- #
+
+
+def searchFile():
+    fnameInput = fileEntry.get()
+    fpathInput = pathEntry.get()
+    searchResult = find_files(fnameInput, fpathInput)
+    text.insert(END, f"{searchResult}")
+
+
+# ---------------------------- Save Function------------------------------- #
+def save():
+    # date time properties needs to use the json.dumps to transfer to the json format(serialize)
+    funcName = "find_files"
+    fnameInput = fileEntry.get()
+    fpathInput = pathEntry.get()
+    searchResult = text.get('1.0', END)
+    recordDate = json.dumps(datetime.now().strftime('%Y-%m-%d'))
+
+    new_data = {
+        funcName: {
+            "fname": fnameInput,
+            "pathname": fpathInput,
+            "searchResult": searchResult,
+            "recordDate": recordDate
+        }
+    }
+    # Todo:  add a check for regex
+    if len(fnameInput) == 0 or len(fpathInput) == 0 or len(searchResult) == 0:
+        messagebox.showinfo(
+            title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        try:
+            with open("json/searchFiles.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("json/searchFiles.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("json/searchFiles.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            #make sure the program runs more smoothly
+            time.sleep(0.5)
+            fileEntry.delete(0, END)
+            pathEntry.delete(0, END)
+            text.delete('1.0', END)
+
+
+# ----------------------------Delete File Funtion------------------------------- #
+def delete():
+    try:
+        os.remove("json/searchFiles.json")
+    except FileNotFoundError:
+        messagebox.showinfo(
+            title="Oops", message="The File hasn't been created yet, nothing to delete")
+
+# ----------------------------Date Query Function------------------------------- #
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+#Global Config
+window = Tk()
+window.title("findFiles Function")
+window.config(padx=35, pady=35)
+
+
+
+# Labels
+fileLabel = Label(window, text="File:")
+fileLabel.grid(row=1, column=0)
+pathLabel = Label(window, text="Path:")
+pathLabel.grid(row=2, column=0)
+textLabel = Label(window, text="Result:")
+textLabel.grid(row=3, column=0)
+
+# Entries
+# fileEntry
+fileEntry = Entry(width=40)
+fileEntry.grid(row=1, column=1)
+fileEntry.focus()
+# path Entry
+pathEntry = Entry(width=40)
+pathEntry.grid(row=2, column=1)
+pathEntry.focus()
+
+
+# Buttons
+search_button = Button(text="Search", width=9, command=searchFile)
+#the first parameter for padx is left, the second parameter for pady is the right
+search_button.grid(row=2, column=2,padx=(3,0))
+
+#the tuple for pady, the first parameter for top, second parameter for buttom
+add_button = Button(text="Save", width=30, command=save)
+add_button.grid(row=4, column=1,pady=(5,5))
+
+delete_button = Button(text="Delete", width=30, command=delete)
+delete_button.grid(row=5, column=1,pady=(0,5))
+
+# Todo This jumps to a new UI window to execute the Query function, there would be some command for switching the UI
+query_button = Button(text="Date Query", width=30)
+query_button.grid(row=6, column=1)
+
+
+# Text
+text = Text(height=5, width=30)
+# Puts cursor in textbox.
+text.focus()
+text.grid(row=3, column=1)
+
+window.mainloop()
---
+++ utility.py
@@ -0,0 +1,243 @@
+import os
+import fnmatch
+from glob import glob
+import shutil
+import datetime
+import time
+
+
+def getSize(filepath: str) -> int:
+    if not os.path.isdir(filepath):
+        return
+    name_list = os.listdir(filepath)
+    size = 0
+    for files in name_list:
+        pathIndex = f'{filepath}/{files}'
+        if os.path.isdir(pathIndex):
+            size += getSize(pathIndex)
+        else:
+            size += os.path.getsize(pathIndex)
+    return size
+
+
+def find_files(filename: str, search_path: str) -> list[str]:
+    if not os.path.isdir(search_path):
+        return []
+    result = []
+# Walking top-down from the root
+    for root, dir, files in os.walk(search_path):
+        if filename in files:
+            result.append(os.path.join(root, filename))
+    return result
+
+#example : find_files("Python1.pdf","C:\\something\\")
+
+
+def findFileByName(filename: str, search_path: str) -> list[str]:
+    if not os.path.isdir(search_path):
+        return []
+    result = []
+    # Walking top-down from the root
+    for root, dir, files in os.walk(search_path):
+        for file in files:
+            # since the splittext function does not accept the list. can I change the function to make it to accept the list?
+            split_name, ext = os.path.splitext(file)
+            if(filename == split_name):
+                file_name = split_name + ext
+                result.append(os.path.join(root, file_name))
+
+    return result
+
+
+def fuzzySearchFile(pattern, search_path):
+    # this function just need to input the fuzzy file name under the specific directory and returns the path which contains the fuzzyName
+
+    # if user doesn't enter the valid search_path, corece to the root path.
+    search_path = "." if search_path == "" else search_path
+    if not os.path.isdir(search_path):
+        return []
+    result = []
+    # Walking top-down from the root
+    for root, dir, files in os.walk(search_path):
+        files = (os.path.join(root, name) for name in files)
+        result.extend(fnmatch.filter(files, f"*{pattern}*"))
+    return result
+
+
+def fuzzySearchFile2(pattern, search_path):
+    # this function just need to input the fuzzy file name under the specific directory and returns the path which contains the fuzzyNam
+    abspath = os.path.abspath(search_path)
+    if os.path.exists(abspath):
+        files = glob(os.path.join(abspath, "**", pattern), recursive=True)
+    else:
+        raise Exception(f"{abspath} does not exists")
+    return files
+
+
+def findFileByType(file_type: str, search_path: str) -> list[str]:
+
+    if not os.path.isdir(search_path):
+        return []
+
+    file_type = file_type if file_type[0] == "." else f".{file_type}"
+
+    result = []
+
+    name_list = os.listdir(search_path)
+    for file in name_list:
+        pathIndex = os.path.join(search_path, file)
+        if os.path.isdir(pathIndex):
+            result.extend(findFileByType(file_type, pathIndex))
+        else:
+            _, ext = os.path.splitext(file)
+            if ext == file_type:
+                result.append(pathIndex)
+            else:
+                pass
+
+    return result
+# findFileByType("extention","filePath")
+
+
+def findFileByTypes(search_path: str, *file_type: str) -> list[str]:
+    # this function can allow you to input the multiple types of the postfix type and return their path
+
+    # make sure that the search_path exists
+    if not os.path.isdir(search_path):
+        return []
+
+    file_type = [
+        f'.{ftype}' if ftype[0] != '.' else ftype
+        for ftype in file_type
+    ]
+
+    # declare outcome list
+    result = []
+
+    name_list = os.listdir(search_path)
+    for file in name_list:
+        pathIndex = os.path.join(search_path, file)
+        if os.path.isdir(pathIndex):
+            result.extend(findFileByTypes(pathIndex, *file_type))
+        else:
+            _, ext = os.path.splitext(file)
+            if ext in file_type:
+                result.append(pathIndex)
+            else:
+                pass
+    return result
+
+
+def compare2Folder(path1: str, path2: str) -> list[str]:
+    # this function compare 2 folders and output the different file name into an array from 2 directories provided
+
+    # turn both path into abs paths
+    abspath1 = os.path.abspath(path1)
+    abspath2 = os.path.abspath(path2)
+
+    # make sure both search path exists
+    if not os.path.isdir(abspath1) or not os.path.isdir(abspath2):
+        return []
+    absList1 = _getAllFiles(abspath1)
+    absList2 = _getAllFiles(abspath2)
+
+    return list(set(absList1) - set(absList2)) + list(set(absList2) - set(absList1))
+
+
+def _getAllFiles(path: str) -> list[str]:
+    if not os.path.isdir(path):
+        return []
+    absPath = os.path.abspath(path)
+
+    # declare the out put list
+    result = []
+    # Walking top-down from the root
+    for root, dir, files in os.walk(absPath):
+        result.extend(files)
+    return result
+
+
+def compare2FolderStrict(path1: str, path2: str) -> list[str]:
+    # similar to compare2Folder, but would examine more parameters (etc, filelength)
+    # turn both path into abs paths
+    abspath1 = os.path.abspath(path1)
+    abspath2 = os.path.abspath(path2)
+
+    # make sure both search path exists
+    if not os.path.isdir(abspath1) or not os.path.isdir(abspath2):
+        return []
+
+    # get the file and size pair
+    absDict1 = _getFileAndSize(abspath1)
+    absDict2 = _getFileAndSize(abspath2)
+
+    return list({k: absDict2[k] for k in set(absDict2) - set(absDict1)} | {k: absDict1[k] for k in set(absDict1) - set(absDict2)}.keys())
+
+
+def _getFileAndSize(path: str) -> dict[str, int]:
+    if not os.path.isdir(path):
+        return []
+
+    # declare the output dict
+    outDict = {}
+    # Walking top-down from the root
+    for root, dir, files in os.walk(os.path.abspath(path)):
+        for file in files:
+            filePath = os.path.join(os.path.abspath(root), file)
+            size = os.path.getsize(filePath)
+            outDict[file] = size
+    return outDict
+
+
+def copyUnzip(fromSource, toDestination):
+    # this function copy from the source folder and paste it to the destination folder and unzip
+
+    # 1: copy and paste logic
+    _copy(fromSource, toDestination)
+
+    # separate the fromSource filename and form the toDestination filename
+    fileName = os.path.basename(fromSource)
+    dest = os.path.join(toDestination, fileName)
+    # 3: unzip logic
+    _unzip(dest)
+
+
+def _copy(src, dest):
+    # fromsource must be the file not the directory
+    if not os.path.isfile(src):
+        print("you need to enter the valid file path")
+
+    # if the destination not exists, then create the destination
+    if not os.path.isdir(dest):
+        dest = os.mkdir(dest)
+    shutil.copy2(src, dest)
+
+
+def _unzip(dest):
+    # separate the fromSource filename and form the toDestination filename
+    fileName = os.path.dirname(dest)
+    shutil.unpack_archive(dest, fileName)
+
+
+def batchPrintingByType(file_type, file_path):
+    # this function will print all the file type under the specific path
+    pass
+
+
+def recentUpfiles(path):
+    # this function get the most recent updated files from a directory
+    if not os.path.isdir(path):
+        return []
+    # declare the output
+    outFile = []
+    # Walking top-down from the root
+    for root, dir, files in os.walk(os.path.abspath(path)):
+        for file in files:
+            fileAbsPath = os.path.abspath(os.path.join(root, file))
+            fileModifiedTime = os.path.getmtime(fileAbsPath)
+            timeAgo = time.time() - fileModifiedTime
+            sevenDays = datetime.timedelta(days=7).total_seconds()
+            if timeAgo <= sevenDays:
+                filePath = os.path.join(os.path.abspath(root), file)
+                outFile.extend(filePath)
+    return outFile
